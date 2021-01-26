3236e7fe51d54ffbdf01e93bee73bc985ed05daa
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2021-01-27 00:40:58 +0300

    Generics

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<T, H>
+    {
+        T[] items;
+        T[] tempItems;
+
+        H[] piece;
+        H[] tempPiece;
+        int sum = 0;
+
+        // When class is called, CONSTRUCTOR runs first.
+
+        public MyList()
+        {
+            items = new T[0];
+            piece = new H[0];
+        }
+
+        public void Add(T item, H stock)
+        {
+            tempItems = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempItems.Length; i++)
+            {
+                items[i] = tempItems[i];
+            }
+            items[items.Length - 1] = item;
+
+            tempPiece = piece;
+            piece = new H[piece.Length+1];
+            for (int i = 0; i < tempPiece.Length; i++)
+            {
+                piece[i] = tempPiece[i];
+            }
+            piece[piece.Length - 1] = stock;
+            
+        }
+
+
+        public T[] FruitCount
+        {
+            get { return items; }
+        }
+
+        public H[] FruitPiece
+        {
+            get { return piece; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            //List<string> fruits0 = new List<string>();
+            //fruits0.Add("Orange");
+            //Console.WriteLine(fruits0[0]);
+
+            MyList<string, int> fruits = new MyList<string, int>();
+
+            fruits.Add("Apple", 12);
+            fruits.Add("Orange", 6);
+            fruits.Add("Birne", 6);
+            fruits.Add("Banana", 12);
+            fruits.Add("Melon", 3);
+
+            for (int i = 0; i < fruits.FruitCount.Length; i++)
+            {
+                sum += fruits.FruitPiece[i];
+            }
+
+            for (int i = 0; i < fruits.FruitCount.Length; i++)
+            {
+                Console.WriteLine("Fruit Name: " + fruits.FruitCount[i] + " --" + " Piece: " + fruits.FruitPiece[i]);
+            }
+
+            Console.WriteLine("Total amount of fruit: " + sum);
+        }
+    }
+}
